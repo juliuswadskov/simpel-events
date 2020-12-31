bb77cef4bc8911bb88268e671d1d2d16d69078c0
Raag2005 <oskar@raagart.dk>
2020-12-31 21:22:10 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,79 @@
+--    _____                   ___   ___   ___  _____   _  _    ___   ___   ___  __ 
+--   |  __ \                 |__ \ / _ \ / _ \| ____|_| || |_ / _ \ / _ \ / _ \/_ |
+--   | |__) |__ _  __ _  __ _   ) | | | | | | | |__ |_  __  _| | | | | | | | | || |
+--   |  _  // _` |/ _` |/ _` | / /| | | | | | |___ \ _| || |_| | | | | | | | | || |
+--   | | \ \ (_| | (_| | (_| |/ /_| |_| | |_| |___) |_  __  _| |_| | |_| | |_| || |
+--   |_|  \_\__,_|\__,_|\__, |____|\___/ \___/|____/  |_||_|  \___/ \___/ \___/ |_|
+--                       __/ |                                                     
+--                      |___/    
+--                                                  
+--  This is som simpel events for FiveM
+--  Copyright, Oskar Raagart at https://www.raag2005.dk
+
+RegisterNetEvent('raag:blip')
+RegisterNetEvent('raag:helpText')
+RegisterNetEvent('raag:setView')
+RegisterNetEvent('raag:addPed')
+
+-- Add Ped
+AddEventHandler('raag:addPed', function(x,y,z,h, ped) 
+    local ped1 = GetHashKey(ped)
+    RequestModel(ped1)
+    while not HasModelLoaded(ped1) do
+        Citizen.Wait(5000)
+    end
+    pedId = CreatePed(5, ped1, x,y, z, h, false, true)
+    SetPedCombatAttributes(pedId, 46, true)
+    SetPedFleeAttributes(pedId, 0, 0)
+    SetBlockingOfNonTemporaryEvents(pedId, true)
+    SetEntityAsMissionEntity(pedId, true, true)
+    SetEntityInvincible(pedId, true)
+    FreezeEntityPosition(pedId, true)
+end)
+
+-- Set View
+AddEventHandler('raag:setView', function(x,y,z, h, bool)
+    if bool == true then
+        function ChangeView(x, y, z)
+            local cam = CreateCam("DEFAULT_SCRIPTED_CAMERA", 1)
+            if createdCamera ~= 0 then
+                DestroyCam(createdCamera, 0)
+                createdCamera = 0
+            end
+            SetCamCoord(cam, x, y, z)
+            SetCamRot(cam, 0.0, 0.0, h)
+            RenderScriptCams(1, 0, 0, 1, 1)
+            Citizen.Wait(250)
+            createdCamera = cam
+        end
+    elseif bool == false then
+        function StopView()
+            DestroyCam(createdCamera, 0)
+            RenderScriptCams(0, 0, 1, 1, 1)
+            createdCamera = 0
+            SetFocusEntity(GetPlayerPed(PlayerId()))
+            FreezeEntityPosition(GetPlayerPed(PlayerId()), false)
+        end
+    end
+end)
+
+-- Help text
+AddEventHandler('raag:helpText', function(text) 
+    ClearAllHelpMessages();
+    SetTextComponentFormat("STRING");
+    AddTextComponentString(text);
+    DisplayHelpTextFromStringLabel(0, false, true, 5000);
+end)
+
+-- blip
+AddEventHandler('raag:blip', function(text, x, y, z) 
+    local blip = AddBlipForCoord(x, y, z)
+    SetBlipScale(blip, 1.25)
+    SetBlipDisplay(blip, 4)
+    SetBlipSprite(blip, 430)
+    SetBlipColour(blip, 3)
+    SetBlipAsShortRange(blip, true)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString(text)
+    EndTextCommandSetBlipName(blip)
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'Raag2005#0001'
+description 'Free to use variables (simpel version)'
+version '1.0'
+
+client_script 'client.compressed.lua' 
